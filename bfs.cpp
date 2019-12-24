95ee4d7deedd015d4e72f84d7f18615f2e07761b
Arjun Jaiswal <arjunjaiswal775@gmail.com>
2019-12-24 14:56:02 +0530

    Update and rename bfs.txt to bfs.cpp

---
+++ bfs.cpp
@@ -0,0 +1,14 @@
+ll bfs(ll a,ll b){
+    queue<ll>q;q.push(a);q.push(b);ar[a][b]=0;
+    ll ax[]={1,0,-1,0}; ll ay[]={0,1,0,-1};
+    while(!q.empty()){
+        ll u=q.front();q.pop();
+        ll v=q.front();q.pop();
+        ll i,j;
+        for(i=0;i<4;i++){
+            ll uu=ax[i]+u;ll vv=ay[i]+v;
+            if(check(uu,vv)){
+                    if(ar[u][v]+1<ar[uu][vv]){
+                        ar[uu][vv]=ar[u][v]+1;
+                        q.push(uu);
+                        q.push(vv);}}}}}
