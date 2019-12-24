c513ae3261f63f26efee610a88ab9adb9ff6a820
Arjun Jaiswal <arjunjaiswal775@gmail.com>
2019-12-24 14:46:32 +0530

    Update LCA in O(logn).cpp

--- LCA in O(logn).cpp
+++ LCA in O(logn).cpp
@@ -1,45 +1,28 @@
-void dfss(ll cur, ll prev)
-{
+void dfss(ll cur, ll prev){
     lev[cur] = lev[prev] + 1;
     parent[cur][0] = prev;
-    for (ll i=0; i<v[cur].size(); i++)
-    {
+    for (ll i=0; i<v[cur].size(); i++){
         if (v[cur][i] != prev)
-            dfss(v[cur][i], cur);
-    }
-}
-void precomputeSparseMatrix(ll n)
-{
+            dfss(v[cur][i], cur);}}
+void precomputeSparseMatrix(ll n){
     ll kk=20;
-    for (ll i=1; i<kk; i++)
-    {
-        for (ll node = 1; node <= n; node++)
-        {
+    for (ll i=1; i<kk; i++){
+        for (ll node = 1; node <= n; node++){
             if (parent[node][i-1] != -1)
                 parent[node][i] =
-                    parent[parent[node][i-1]][i-1];
-        }
-    }
-}
-ll lca(ll u, ll v)
-{
+                    parent[parent[node][i-1]][i-1];}}}
+ll lca(ll u, ll v){
     ll kk=20;
     if (lev[v] < lev[u])
         swap(u, v);
-
     ll diff = lev[v] - lev[u];
-
     for (ll i=0; i<kk; i++)
         if ((diff>>i)&1)
             v = parent[v][i];
     if (u == v)
         return u;
     for (ll i=kk-1; i>=0; i--)
-        if (parent[u][i] != parent[v][i])
-        {
+        if (parent[u][i] != parent[v][i]){
             u = parent[u][i];
-            v = parent[v][i];
-        }
-
-    return parent[u][0];
-}
+            v = parent[v][i];}
+    return parent[u][0];}
